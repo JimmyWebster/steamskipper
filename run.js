19d4e909c762ad86254f2468f1811798d40903d3
Jimmy Webster <70374273+JimmyWebster@users.noreply.github.com>
2020-08-29 02:42:50 +0300

    Update run.js

--- run.js
+++ run.js
@@ -1,17 +1,20 @@
 (function() {
     'use strict';
 
-    console.log('Steam Auto Queue Loaded - Will try to click Next automatically...');
+    console.log('Automatically skip games');
     
 	try {
         if (window.location.pathname.indexOf('/agecheck') > -1) {
             var appid = window.location.pathname.replace('/agecheck', '').replace('/app/', '');
             HideAgeGate(appid);
-        } else {
+        } else 
             $J('#next_in_queue_form').submit();
-        }
+   
 	}
 	catch(err) {
-		console.log('Error clicking next.');
-	}
-})();
+		console.log('Error');}
+}
+)
+(
+)
+;
